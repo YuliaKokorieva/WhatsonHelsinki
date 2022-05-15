9b6eead618a530427319dc0dda6feb6d0d75f6bb
YuliaKokorieva <yuliya.pastushenko@gmail.com>
2022-05-15 18:22:47 +0200

    styles refacroting

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -8,11 +8,19 @@ export const globalStyles = StyleSheet.create({
     padding: 5,
   },
   rowStyle: {
-    height: 50,
-    alignItems: 'center'
+    height: 48,
+    alignItems: 'center',
+    marginHorizontal: 5
   },
   textField: {
     width: 60, 
+    borderColor: 'black', 
+    borderWidth: 1, 
+    margin: 5,
+    backgroundColor: 'white'
+  },
+  textFieldWide: {
+    width: 200, 
     borderColor: 'black', 
     borderWidth: 1, 
     margin: 5,
@@ -25,7 +33,20 @@ export const globalStyles = StyleSheet.create({
   },
   header: {
     fontSize: 20
+  },
+  listItemSeparator: {
+    height: 1,
+    width: "80%",
+    backgroundColor: "#CED0CE",
+    marginLeft: "10%"
+  },
+  agendaCard: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   }
+  
+
 
 
   
